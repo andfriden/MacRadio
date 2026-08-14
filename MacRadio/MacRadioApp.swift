bb80621d24b3e4b0c97bc268eee2402746c42f9d
Anders Friden <and,friden@yandex.ru>
2026-08-14 16:54:47 +0300

    Initial Commit

---
+++ MacRadio/MacRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacRadioApp.swift
+//  MacRadio
+//
+//  Created by Андерс Фриден on 14.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct MacRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
